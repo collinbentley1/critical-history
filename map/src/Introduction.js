b600fa841074df2c9ea75d4d1e0fa0adfea5f1c2
Collin Bentley <collin.bentley@me.com>
2020-11-22 16:52:27 -0500

    Button styling

--- map/src/Introduction.js
+++ map/src/Introduction.js
@@ -36,10 +36,10 @@ function Introduction() {
       <h1>Critical History Map</h1>
       Yale University’s namesake is Elihu Yale, a slave trader and the governor of the British East India Company responsible for over a century of colonial rule in India. This Critical History Map was developed with two ambitions in mind: (1) to think about how Yale’s history as a colonial institution remains embedded in its architecture and landscape in the present-day and (2) to highlight sites where Yale students and New Haven residents have changed the course of the university’s history through remarkable moments of struggle.</Modal.Body>
       <Modal.Footer className="justify-content-center">
-        <Button variant='secondary' onClick={handleClose}>
+        <Button variant='outline-secondary' onClick={handleClose}>
         Explore 
         </Button>
-        <Button variant='primary' onClick={handleGuided}>
+        <Button variant='outline-primary' onClick={handleGuided}>
         Guided Tour
         </Button>
       </Modal.Footer>
