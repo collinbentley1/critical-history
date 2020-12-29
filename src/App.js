de2046c6d0b839ec24a84e75c04f29d263516372
Collin Bentley <collin.bentley@me.com>
2020-12-29 13:48:57 -0500

    Remove react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
-    );
+      <Home />
+    )
   }
 }
 
