ab5d92c7aa05ab8b8c6a5a60bddd6f2328bd99c8
Collin Bentley <collin.bentley@me.com>
2020-12-29 17:49:08 -0500

    Load privacy only on mount

--- src/AboutPrivacy.js
+++ src/AboutPrivacy.js
@@ -1,24 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import privacy from './privacy.md';
+
+
 
 const AboutPrivacy = () => {
     // Declare state variable, for markdown
     const [ mdText, setText ] = useState({markdown: ''});
-    fetch(privacy).then(res => res.text()).then(text => setText({ markdown: text }));
 
+    useEffect( () => {
+        fetch(privacy).then(res => res.text()).then(text => setText({ markdown: text }));
 
-    // Link renderer: allow links to open in new tab
+    }, []);
+
+    //Link renderer: allow links to open in new tab
     function LinkRenderer(props) {
         return <a href={props.href} target="_blank" rel="noopener noreferrer">{props.children}</a>
     }
+
+    const mdComponent = <ReactMarkdown 
+                            source={mdText.markdown}
+                            renderers={{link: LinkRenderer}}
+                        />
  
     return (
         <div className="pl-5 pr-5 mb-3 about-text">
-            <ReactMarkdown 
-                source={mdText.markdown}
-                renderers={{link: LinkRenderer}}
-            />
+            {mdComponent}
         </div>
     )
 }
