01ae178b6d48a514513a4d770c1e3a08310503e5
collinbentley1 <collin.bentley@me.com>
2026-08-21 23:49:02 -0400

    chore(platform): forward protected environment slots

--- infra/terraform/bootstrap/main.tf
+++ infra/terraform/bootstrap/main.tf
@@ -1,5 +1,5 @@
 module "bootstrap" {
-  source = "github.com/collinbentley1/platform//terraform/modules/bootstrap?ref=b599700859765d09969b0aa1ea3689cbee9f532e"
+  source = "github.com/collinbentley1/platform//terraform/modules/bootstrap?ref=eb48f5f4e321b81bc033e1113bfe2f9e676f1484"
 
   app                         = "critical-history"
   project_id                  = var.project_id
@@ -12,7 +12,7 @@ module "bootstrap" {
   github_owner_id             = var.github_owner_id
   github_repository_id        = var.github_repository_id
   trusted_platform_workflow_shas = [
-    "b599700859765d09969b0aa1ea3689cbee9f532e",
+    "eb48f5f4e321b81bc033e1113bfe2f9e676f1484",
   ]
   legacy_compatibility_mode                              = false
   manage_automatic_default_service_account_grants_policy = var.manage_automatic_default_service_account_grants_policy
--- infra/terraform/prod/main.tf
+++ infra/terraform/prod/main.tf
@@ -1,5 +1,5 @@
 module "site" {
-  source = "github.com/collinbentley1/platform//terraform/modules/cloud-run-service?ref=b599700859765d09969b0aa1ea3689cbee9f532e"
+  source = "github.com/collinbentley1/platform//terraform/modules/cloud-run-service?ref=eb48f5f4e321b81bc033e1113bfe2f9e676f1484"
 
   providers = {
     google                = google
